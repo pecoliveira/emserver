4b493ab9233ed8dd9ef8003f9325fa4069f03c91
pecoliveira <pecoliveira0@icloud.com>
2023-10-03 09:22:58 -0300

    updated some things

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
+const state = true;
+
 
 export default function Home() {
   return (
+    <div>
     <h1 className='font-bold'>Home</h1>
+    <Button>click Me</Button>
+    </div> 
   )
 }
