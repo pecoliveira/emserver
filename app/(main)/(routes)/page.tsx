74e4e31d9eb3491a7bb278e5bc9eee8393b42849
pecoliveira <pecoliveira0@icloud.com>
2023-10-03 15:48:01 -0300

    added clerk functionality

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button"
-
-const state = true;
-
 
 export default function Home() {
   return (
     <div>
     <h1 className='font-bold'>Home</h1>
-    <Button>click Me</Button>
+    click Me
     </div> 
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -14,9 +15,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body className={font.className}>{children}</body>
     </html>
+    
+    </ClerkProvider>
+    
   )
+  
 }
